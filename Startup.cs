706b3065f6200b3d63d13a393964a5a7e07b8fc4
prashantisapure <prashant@sdglobaltech.com>
2020-06-08 10:55:18 +0800

    Sample code

---
+++ Startup.cs
@@ -0,0 +1,161 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Autofac;
+using Autofac.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using HealthChecks.UI.Client;
+using Campaigns.API.Infrastructure;
+
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+using Campaigns.API.Controllers;
+using Microsoft.OpenApi.Models;
+using System;
+using System.Reflection;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace Campaigns.API
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public virtual IServiceProvider ConfigureServices(IServiceCollection services)
+        {
+           
+
+          
+            services.AddCustomHealthCheck(Configuration);
+            services
+                .AddControllers()               
+                .AddApplicationPart(typeof(CampaignsController).Assembly)
+                .AddNewtonsoftJson()
+                .SetCompatibilityVersion(CompatibilityVersion.Version_3_0);
+            services.Configure<MarketingSettings>(Configuration);
+
+            
+
+            services.AddEntityFrameworkSqlServer()
+                .AddDbContext<MarketingContext>(options =>
+            {
+                options.UseSqlServer(Configuration["ConnectionString"],
+                                     sqlServerOptionsAction: sqlOptions =>
+                                     {
+                                         sqlOptions.MigrationsAssembly(typeof(Startup).GetTypeInfo().Assembly.GetName().Name);                                         
+                                         sqlOptions.EnableRetryOnFailure(maxRetryCount: 15, maxRetryDelay: TimeSpan.FromSeconds(30), errorNumbersToAdd: null);
+                                     });               
+                options.ConfigureWarnings(warnings => warnings.Throw(RelationalEventId.QueryClientEvaluationWarning));
+              
+            });
+
+            AddCustomSwagger(services);
+            services.AddCors(options =>
+            {
+                options.AddPolicy("CorsPolicy",
+                    builder => builder
+                    .SetIsOriginAllowed((host) => true)
+                    .AllowAnyMethod()
+                    .AllowAnyHeader()
+                    .AllowCredentials());
+            });
+
+            
+
+           
+            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+            
+
+            services.AddOptions();
+
+            //configure autofac
+            var container = new ContainerBuilder();
+            container.Populate(services);
+
+            return new AutofacServiceProvider(container.Build());
+        }
+
+      public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ILoggerFactory loggerFactory)
+        {
+           
+
+            var pathBase = Configuration["PATH_BASE"];
+
+            if (!string.IsNullOrEmpty(pathBase))
+            {
+                app.UsePathBase(pathBase);
+            }
+
+            app.UseCors("CorsPolicy");
+            app.UseRouting();
+
+           
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapDefaultControllerRoute();
+                endpoints.MapControllers();
+                endpoints.MapHealthChecks("/hc", new HealthCheckOptions()
+                {
+                    Predicate = _ => true,
+                    ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+                });
+                endpoints.MapHealthChecks("/liveness", new HealthCheckOptions
+                {
+                    Predicate = r => r.Name.Contains("self")
+                });
+            });
+
+            app.UseSwagger()
+               .UseSwaggerUI(setup =>
+               {
+                   setup.SwaggerEndpoint($"{ (!string.IsNullOrEmpty(pathBase) ? pathBase : string.Empty) }/swagger/v1/swagger.json", "Marketing.API V1");
+                 
+               });
+
+          
+        }
+
+        private void AddCustomSwagger(IServiceCollection services)
+        {
+            services.AddSwaggerGen(options =>
+             {
+                 options.DescribeAllEnumsAsStrings();
+                 options.SwaggerDoc("v1", new OpenApiInfo
+                 {
+                     Title = "HTTP API",
+                     Version = "v1",
+                     Description = " HTTP API"
+                 });                
+
+                
+             });
+        }
+
+
+       
+
+       
+    }
+
+    public static class CustomExtensionMethods
+    {
+        public static IServiceCollection AddCustomHealthCheck(this IServiceCollection services, IConfiguration configuration)
+        {
+            var hcBuilder = services.AddHealthChecks();
+
+           // Configuration code need to write later
+            return services;
+        }
+    }
+}
